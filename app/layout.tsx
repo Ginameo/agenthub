9d908a18d707f858f37e56592215aa6824164930
Ginameo <ginameo@users.noreply.github.com>
2026-05-29 14:35:15 +0000

    fix: adjust main padding-top to pt-[80px]

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
       <body className={`${audiowide.variable} ${jetbrainsMono.variable} antialiased min-h-screen flex flex-col`}>
         <Providers>
           <Navbar />
-          <main className="flex-1 pt-20">
+          <main className="flex-1 pt-[80px]">
             {children}
           </main>
           <Footer />
